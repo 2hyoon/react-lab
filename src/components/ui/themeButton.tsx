9592487c6ed2021daa51bb25874509cc7311862e
hyoon <12157850+2hyoon@users.noreply.github.com>
2026-03-17 14:12:28 -0400

    refactor(ui): simplify ThemeButton icon rendering

--- src/components/ui/themeButton.tsx
+++ src/components/ui/themeButton.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from "@/src/hooks/themeContext";
 import { Theme } from "@/src/types/type";
+import { Moon, Sun } from "lucide-react";
 
 const ThemeButton = () => {
   const { theme, toggleTheme } = useTheme();
@@ -10,38 +11,12 @@ const ThemeButton = () => {
     <button
       onClick={toggleTheme}
       aria-label="Toggle theme"
-      className="p-3 rounded-full bg-foreground-light dark:bg-foreground-dark border border-border-light dark:border-border-dark shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-light dark:focus:ring-primary-dark focus:ring-offset-background-light dark:focus:ring-offset-background-dark transition-all duration-300"
+      className="fixed top-2.5 right-4 p-1 rounded-full bg-foreground-light dark:bg-foreground-dark hover:bg-gray-500 focus:outline-none transition-all duration-300 cursor-pointer"
     >
       {theme === Theme.LIGHT ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6 text-yellow-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-          />
-        </svg>
+        <Sun width={20} height={20}/>
       ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6 text-blue-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          />
-        </svg>
+        <Moon width={20} height={20}/>
       )}
     </button>
   );
