a7529a6c40a1fd3b30fb9533ce58180bd0faa3c0
hyoon <12157850+2hyoon@users.noreply.github.com>
2026-03-17 14:15:54 -0400

    feat(layout): add responsive sidebar toggle and Space Mono theme fonts

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import ThemeButton from "@/src/components/ui/themeButton";
+import { Menu, X } from "lucide-react";
+
+interface HeaderProps {
+  isOpen: boolean;
+  onToggleNav: () => void;
+}
+
+const Header = ({ isOpen, onToggleNav }: HeaderProps) => {
+  return (
+    <header className="fixed w-dvw h-12 bg-gray-900 border-b border-b-gray-700 z-50">
+      <button onClick={onToggleNav} className="relative top-1 left-1 bg-gray-900 w-10 h-10 flex justify-center items-center cursor-pointer">
+        {isOpen ? <X size={24} /> : <Menu size={24} />}
+      </button>
+      <ThemeButton />
+    </header>
+  );
+};
+
+export default Header;
--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// import { isPagesAPIRouteMatch } from "next/dist/server/route-matches/pages-api-route-match";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 // import clsx from "clsx";
@@ -20,27 +21,39 @@ const links = [
   },
 ];
 
-export default function Nav() {
+interface NavProps {
+  isOpen: boolean;
+}
+
+export default function Nav({ isOpen }: NavProps) {
   const pathname = usePathname();
 
   return (
-    <nav>
-      <ul>
-        {links.map((link) => (
-          <li key={link.name}>
-            <Link
-              href={link.href}
-              // className={clsx(
-              //   "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              //   { "bg-sky-100 text-blue-600": pathname === link.href }
-              // )}
-              className={pathname === link.href ? 'text-blue-600' : 'text-gray-200'}
-            >
-              {link.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div id="aside" className="fixed w-[250px] top-0 left-0 h-dvh lg:relative lg:w-auto lg:col-start-1 lg:row-start-1 bg-gray-900 ">
+      <div className="absolute inset-0">
+        <div className="sticky top-0 bottom-0 left-0 h-full overflow-y-auto pt-14 border-r border-r-gray-700">
+          <nav id="main-nav" className={isOpen ? `open pl-5 font-mono` : `pl-5 font-mono`}>
+            <ul>
+              {links.map((link) => (
+                <li key={link.name} className="py-1 text-g">
+                  <Link
+                    href={link.href}
+                    // className={clsx(
+                    //   "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                    //   { "bg-sky-100 text-blue-600": pathname === link.href }
+                    // )}
+                    className={
+                      pathname === link.href ? "text-green-400" : "text-grey-100"
+                    }
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
   );
 }
